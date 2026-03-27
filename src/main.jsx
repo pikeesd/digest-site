93260b2b9e901809cd514904d390c69e5c02fd11
Maksym <maksym@MacBook-Air-Maksym.local>
2026-03-27 12:49:19 +0100

    add analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
+import { Analytics } from '@vercel/analytics/react'
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
